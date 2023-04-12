cc73bbd1a3b06fdf3f4da815218345c48dc7fc73
branhickman <branhickman@gmail.com>
2023-04-12 11:11:31 -0600

    add slidetoggle notes

--- js/toggle-practice.js
+++ js/toggle-practice.js
@@ -1,5 +1,25 @@
 $(document).ready(function(){
   $("#menu-toggle").click(function() {
-    $("#side-nav").toggle(1000);
+    $("#side-nav").slideToggle(1000);
+  });
+
+  $(".info-title").click(function(){
+    $(".info-content").slideToggle("slow")
   });
 })
+
+//! effects covered 
+// toggle //  $("#side-nav").toggle(1000);
+// fadeIn
+// fadeOut
+// fadeTo // for moving to a certain opacity
+  // $("#side-nav").fadeTo(1000, 0.5, function(){
+    // alert("hi there")
+  // });
+// slideUp
+// slideDown
+// slideToggle("slow")
+// slideToggle("fast")
+// slideToggle("fast", function(){
+// alert("hi there")
+  // })
